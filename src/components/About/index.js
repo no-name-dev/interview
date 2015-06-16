4cdf7251213ac89004941573246d5526c66792d2
Anonymous <john@doe.com>
2015-06-16 18:05:26 +0200

    styled about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,14 @@ export default class About extends Component {
     render () {
         return (
             <section id="about" className={styles.Component}>
-                About
+                <div className={styles.Text}>
+                    <h2 className={styles.Heading}>What is Posli</h2>
+                    <p className={styles.Description}>
+                        Posli is platform that combines the best of instant messaging and task-list management and improves your everyday workflow with simple but efficient user interface.
+                    </p>
+                    <button className={styles.Button}>Get in line</button>
+                </div>
+                <div className={styles.Image} />
             </section>
         );
     }
