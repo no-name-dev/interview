eaa03ecdf28e5aa2c08901651208595fcdbeac8b
Anonymous <john@doe.com>
2015-06-16 17:15:02 +0200

    sign-up form styling

--- src/components/common/SignupForm/index.js
+++ src/components/common/SignupForm/index.js
@@ -5,10 +5,10 @@ import styles from  './styles.css';
 export default class SignUpForm extends Component {
     render () {
         return (
-            <div className="Form">
+            <div className={styles.Form}>
                 <form action="/">
-                    <input type="email" placeholder="Input Email for Beta" className="Input -email"/>
-                    <input type="submit" defaultValue="Request" className="Button"/>
+                    <input type="email" placeholder="Input Email for Beta" className={styles.Input}/>
+                    <input type="submit" defaultValue="Request" className={styles.Button}/>
                 </form>
             </div>
         );
