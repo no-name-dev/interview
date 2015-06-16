be62484d6d19e40767eed7b6f62cd94d22363c50
Anonymous <john@doe.com>
2015-06-16 20:11:35 +0200

    styled task management

--- src/components/TaskManagement/index.js
+++ src/components/TaskManagement/index.js
@@ -6,7 +6,14 @@ export default class TaskManagement extends Component {
     render () {
         return (
             <section id="task_management" className={styles.Component}>
-                TaskManagement            
+                <div className={styles.Text}>
+                    <h2 className={styles.Heading}>Task Management</h2>
+                    <p className={styles.Description}>
+                        Give tasks directly through instant messaging and solve them in one time. Don't jump between messengeres and task manageres, stay productive in Posli.
+                    </p>
+                </div>
+                <div className={styles.Iphone}>
+                </div>
             </section>
         );
     }
