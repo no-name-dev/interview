48ec03d152faa45f903b3dda8e48159e4dc32e31
Anonymous <john@doe.com>
2015-06-17 23:10:36 +0200

    styled task management for desktop

--- src/components/TaskManagement/index.js
+++ src/components/TaskManagement/index.js
@@ -5,16 +5,20 @@ import styles from  './styles.css';
 export default class TaskManagement extends Component {
     render () {
         return (
-            <section id="task_management" className={styles.Component}>
-                <div className={styles.Text}>
-                    <h2 className={styles.Heading}>Task Management</h2>
-                    <p className={styles.Description}>
-                        Give tasks directly through instant messaging and solve them in one time. Don't jump between messengeres and task manageres, stay productive in Posli.
-                    </p>
-                </div>
-                <div className={styles.Iphone}>
-                </div>
-            </section>
+            <div className="ComponentWrapper">
+                <section id="task_management" className={styles.Component}>
+                    <div className={styles.Text}>
+                        <h2 className={styles.Heading}>Task Management</h2>
+
+                        <p className={styles.Description}>
+                            Give tasks directly through instant messaging and solve them in one time. Don't jump between
+                            messengeres and task manageres, stay productive in Posli.
+                        </p>
+                    </div>
+                    <div className={styles.Iphone}>
+                    </div>
+                </section>
+            </div>
         );
     }
 }
