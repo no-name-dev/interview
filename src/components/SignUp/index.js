1cf6e572db8982469e5b5a329445f2d55c7b20c5
Anonymous <john@doe.com>
2015-06-17 10:07:20 +0200

    styled sign up

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 
 import styles from  './styles.css';
 
+import SignUpForm from '../common/SignUpForm';
+
 export default class SignUp extends Component {
     render () {
         return (
             <section id="sign_up" className={styles.Component}>
-                SignUp
+                <div className={styles.Text}>
+                    <h2 className={styles.Heading}>Sign up for beta</h2>
+
+                    <div className={styles.Description}>
+                        Sign Up for beta right now and be one of the firsts. With Posli you fix your
+                        workflow and become more efficient.
+                    </div>
+                </div>
+                <div className={styles.Form}>
+                    <SignUpForm />
+                </div>
+                <div className={styles.Share}>
+                    <span className={styles.Text}>Share on</span>
+                    <ul className={styles.SocialServices}>
+                        <li className={styles.Service}>
+                            <button className={styles.Facebook}>Facebook</button>
+                        </li>
+                        <li className={styles.Service}>
+                            <button className={styles.Twitter}>Twitter</button>
+                        </li>
+                    </ul>
+                </div>
             </section>
         );
     }
