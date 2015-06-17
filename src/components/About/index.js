acaadd4486ae1cb5e1517fc4ea7f2914c460423b
Anonymous <john@doe.com>
2015-06-17 22:05:30 +0200

    styled about for desktop

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,16 +5,20 @@ import styles from  './styles.css';
 export default class About extends Component {
     render () {
         return (
-            <section id="about" className={styles.Component}>
-                <div className={styles.Text}>
-                    <h2 className={styles.Heading}>What is Posli</h2>
-                    <p className={styles.Description}>
-                        Posli is platform that combines the best of instant messaging and task-list management and improves your everyday workflow with simple but efficient user interface.
-                    </p>
-                    <button className={styles.Button}>Get in line</button>
-                </div>
-                <div className={styles.Image} />
-            </section>
+            <div className={styles.ComponentWrapper}>
+                <section id="about" className={styles.Component}>
+                    <div className={styles.Text}>
+                        <h2 className={styles.Heading}>What is Posli</h2>
+
+                        <p className={styles.Description}>
+                            Posli is platform that combines the best of instant messaging and task-list management and
+                            improves your everyday workflow with simple but efficient user interface.
+                        </p>
+                        <button className={styles.Button}>Get in line</button>
+                    </div>
+                    <div className={styles.Image}/>
+                </section>
+            </div>
         );
     }
 }
--- src/components/Everywhere/index.js
+++ src/components/Everywhere/index.js
@@ -5,47 +5,54 @@ import styles from  './styles.css';
 export default class Everywhere extends Component {
     render () {
         return (
-            <section id="everywhere" className={styles.Component}>
-                <div className={styles.Text}>
-                    <h2 className={styles.Heading}>
-                        Everywhere
-                    </h2>
-                    <div className={styles.Description}>
-                        Android, iOS, Mac, PC, Web... work everywhere you want to. You'll be always ready with our responsive web app and iOS and Android app.
+            <div className="ComponentWrapper">
+                <section id="everywhere" className={styles.Component}>
+                    <div className={styles.Text}>
+                        <h2 className={styles.Heading}>
+                            Everywhere
+                        </h2>
+
+                        <div className={styles.Description}>
+                            Android, iOS, Mac, PC, Web... work everywhere you want to. You'll be always ready with our
+                            responsive web app and iOS and Android app.
+                        </div>
                     </div>
-                </div>
-                <div className={styles.Targets}>
-                    <ul className={styles.TargetList}>
-                        <li className={styles.Target}>
-                            <div className={styles.Mobile} />
-                            <h3 className={styles.Heading}>
-                                Mobile Apps
-                            </h3>
-                            <p className={styles.Description}>
-                                iOS, Android
-                            </p>
-                        </li>
-                        <li className={styles.Target}>
-                            <div className={styles.Desktop} />
-                            <h3 className={styles.Heading}>
-                                Desktop App
-                            </h3>
-                            <p className={styles.Description}>
-                                Mac, PC
-                            </p>
-                        </li>
-                        <li className={styles.Target}>
-                            <div className={styles.Web} />
-                            <h3 className={styles.Heading}>
-                                Web App
-                            </h3>
-                            <p className={styles.Description}>
-                                Responsive website
-                            </p>
-                        </li>
-                    </ul>
-                </div>
-            </section>
+                    <div className={styles.Targets}>
+                        <ul className={styles.TargetList}>
+                            <li className={styles.Target}>
+                                <div className={styles.Mobile}/>
+                                <h3 className={styles.Heading}>
+                                    Mobile Apps
+                                </h3>
+
+                                <p className={styles.Description}>
+                                    iOS, Android
+                                </p>
+                            </li>
+                            <li className={styles.Target}>
+                                <div className={styles.Desktop}/>
+                                <h3 className={styles.Heading}>
+                                    Desktop App
+                                </h3>
+
+                                <p className={styles.Description}>
+                                    Mac, PC
+                                </p>
+                            </li>
+                            <li className={styles.Target}>
+                                <div className={styles.Web}/>
+                                <h3 className={styles.Heading}>
+                                    Web App
+                                </h3>
+
+                                <p className={styles.Description}>
+                                    Responsive website
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
         );
     }
 }
