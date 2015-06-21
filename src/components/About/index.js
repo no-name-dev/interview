d197b43a4d40716de47ca0a9562e302cf9c64279
Anonymous <john@doe.com>
2015-06-21 11:12:18 +0200

    added easing scroll to navigation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,19 +5,22 @@ import styles from  './styles.css';
 export default class About extends Component {
     render () {
         return (
-            <div className={styles.ComponentWrapper}>
-                <section id="about" className={styles.Component}>
-                    <div className={styles.Text}>
-                        <h2 className={styles.Heading}>What is Posli</h2>
+            <div id="about">
+                <div className={styles.ComponentWrapper}>
+                    <section className={styles.Component}>
+                        <div className={styles.Text}>
+                            <h2 className={styles.Heading}>What is Posli</h2>
 
-                        <p className={styles.Description}>
-                            Posli is platform that combines the best of instant messaging and task-list management and
-                            improves your everyday workflow with simple but efficient user interface.
-                        </p>
-                        <button className={styles.Button}>Get in line</button>
-                    </div>
-                    <div className={styles.Image}/>
-                </section>
+                            <p className={styles.Description}>
+                                Posli is platform that combines the best of instant messaging and task-list management
+                                and
+                                improves your everyday workflow with simple but efficient user interface.
+                            </p>
+                            <button className={styles.Button}>Get in line</button>
+                        </div>
+                        <div className={styles.Image}/>
+                    </section>
+                </div>
             </div>
         );
     }
--- src/components/Header/components/Navigation/index.js
+++ src/components/Header/components/Navigation/index.js
@@ -1,20 +1,8 @@
 import React, {Component} from 'react';
 
-import styles from  './styles.css';
 
-function throttle (func, wait) {
-    wait = wait || 4; // for 60FPS animations
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            func.apply(context, args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
-}
+import styles from  './styles.css';
+import EasingFunction from './easing';
 
 export default class Navigation extends Component {
 
@@ -29,7 +17,7 @@ export default class Navigation extends Component {
             () => {
                 const node = React.findDOMNode(this);
                 const pageOffset = window.pageYOffset;
-                const navOffset = node.parentElement.offsetHeight - node.offsetHeight;
+                const navOffset = node.parentElement.offsetHeight - node.offsetHeight - 10; //when navigating to about, it should already be sticky
 
                 console.log(`Page offset: ${pageOffset}`);
 
@@ -58,6 +46,22 @@ export default class Navigation extends Component {
 
     }
 
+    navigate (event) {
+        event.preventDefault();
+        const navHeight = document.getElementById('navigation').scrollHeight;
+        const targetElement = document.querySelector(event.target.hash);
+
+        const from = window.pageYOffset;
+        const to = targetElement.offsetTop - navHeight;
+
+        animate(from, to, 500, EasingFunction.easeOutCubic, (y) => {
+            window.scrollTo(0, from + y);
+        });
+        setTimeout(() => {
+            window.location.hash = event.target.hash;
+        }, 500);
+    }
+
     render () {
         if (typeof console !== 'undefined') {
             console.log('rendering navigation');
@@ -73,11 +77,57 @@ export default class Navigation extends Component {
             <nav id="navigation" className={navClass}>
                 <ul className={styles.NavigationItems}>
                     <li className={styles.Logo}>Posli</li>
-                    <li className={styles.NavigationItem}><a href="#about">About</a></li>
-                    <li className={styles.NavigationItem}><a href="#features">Features</a></li>
-                    <li className={styles.NavigationItem}><a href="#sign_up">Sign Up</a></li>
+                    <li className={styles.NavigationItem}><a href="#about" onClick={this.navigate}>About</a></li>
+                    <li className={styles.NavigationItem}><a href="#features" onClick={this.navigate}>Features</a></li>
+                    <li className={styles.NavigationItem}><a href="#sign_up" onClick={this.navigate}>Sign Up</a></li>
                 </ul>
             </nav>
         );
     }
 }
+
+function throttle (func, wait) {
+    wait = wait || 4; // for 60FPS animations
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            func.apply(context, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+function animate (from, to, duration, distributionFunc, apply) {
+
+    const FRAME_TIME = 16; //ms for 60FPS animations
+    const requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        ((f) => {
+            setTimeout(f, FRAME_TIME)
+        });
+
+    const frames = Math.floor(duration / FRAME_TIME);
+    const valRange = to - from;
+
+    let frame = 0;
+
+    function frameAnim () {
+        if (frame > frames) {
+            return;
+        }
+        frame += 1;
+
+        const nextVal = Math.floor(distributionFunc(frame / frames) * valRange);
+        apply(nextVal);
+
+        requestAnimationFrame(frameAnim);
+    }
+
+    frameAnim();
+}
+
